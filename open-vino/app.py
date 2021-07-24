9bb28c8c3e3c7b86fdb3273b7941773e2ee05156
Acchindra Thev <acchinthev@gmail.com>
2021-07-23 20:21:58 -0700

    Initial commit

---
+++ open-vino/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST', 'GET'])
+def main():
+    return render_template('app.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
